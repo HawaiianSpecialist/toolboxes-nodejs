7a90e9ba036f560d4f47e24094b3cc0d63b66d16
Jason <jason@kitsapdata.com>
2019-08-01 06:30:28 -0700

    Formatted text and updated .gitignore

--- app.js
+++ app.js
@@ -5,26 +5,33 @@ const port = 3000
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    user: "toolboxes",
-    password: "toolbox",
-    database: "toolboxes"
-  });
+  host: "localhost",
+  user: "toolboxes",
+  password: "toolbox",
+  database: "toolboxes"
+});
 
-  connection.connect(function(err) {
-    if (err) throw err;
+connection.connect(function (err)
+{
+  if (err)
+  {
+    throw err;
+  }
+  else
+  {
     console.log("Connected!");
-  });
+  }
+});
 
-  
 
-  // Routes
+
+// Routes
 app.get('/', (req, res) => res.sendfile('index.html'))
 
-app.get('/add', function(req, res)
-{      
-    console.log(req.query);
-    add(req.query);
+app.get('/add', function (req, res)
+{
+  add(req.query);
+  console.log(req.query);
 });
 
 
@@ -32,14 +39,15 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 function add(query)
 {
-    // Parse query
-    var sqlStatement = "INSERT INTO tools (toolName, toolSize, toolManufacturer, toolDescription) VALUES ('" + query.toolName + "', '" + query.toolSize + "', '" + query.toolManufacturer + "', '" + query.toolDescription + "')";
+  // Parse query
+  var sqlStatement = "INSERT INTO tools (toolName, toolSize, toolManufacturer, toolDescription) VALUES ('" + query.toolName + "', '" + query.toolSize + "', '" + query.toolManufacturer + "', '" + query.toolDescription + "')";
 
-    connection.query(sqlStatement, function (err, rows, fields) {
-      if (err) throw err
-    
-      console.log('The solution is: ', rows[0])
-    })
+  connection.query(sqlStatement, function (err, rows, fields)
+  {
+    if (err) throw err
 
-    console.log("finished");
+    console.log('The solution is: ', rows[0])
+  })
+
+  console.log("finished");
 }
