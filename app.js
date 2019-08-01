443c6106f33983ae0aa50a5926ee035426a18e61
Jason <jason@shinigami.lan>
2019-08-01 07:25:20 -0700

    Refactored mysql connection info and variable names

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// Turn off for production
+var debug = true;
+
 const express = require('express')
 const app = express()
 const port = 3000
@@ -5,8 +8,8 @@ const port = 3000
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  user: "toolboxes",
+  host: "Server",
+  user: "toolbox",
   password: "toolbox",
   database: "toolboxes"
 });
@@ -40,7 +43,7 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 function add(query)
 {
   // Parse query
-  var sqlStatement = "INSERT INTO tools (toolName, toolSize, toolManufacturer, toolDescription) VALUES ('" + query.toolName + "', '" + query.toolSize + "', '" + query.toolManufacturer + "', '" + query.toolDescription + "')";
+  var sqlStatement = "INSERT INTO tools (name, size, manufacturer, description) VALUES ('" + query.name + "', '" + query.size + "', '" + query.manufacturer + "', '" + query.description + "')";
 
   connection.query(sqlStatement, function (err, rows, fields)
   {
@@ -51,3 +54,26 @@ function add(query)
 
   console.log("finished");
 }
+
+function getTools()
+{
+  var statement = "";
+
+  connection.query(statement, function(err, rows, fields)
+  {
+    if(err) throw err
+
+    console.log(rows);
+
+    return rows;
+  });
+}
+
+// For testing purposes
+if (debug)
+{
+  var url = 'http://localhost:3000';
+  var start = (process.platform == 'darwin' ? 'open' : process.platform == 'win32' ? 'start' : 'xdg-open');
+  require('child_process').exec(start + ' ' + url);
+}
+
